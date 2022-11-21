617a958f178b37e0f5d025ea3799a8fc6010ace4
AndyRCR <andycr321@gmail.com>
2022-11-21 18:43:59 -0500

    Safari v1

--- src/context/GlobalStateContext.jsx
+++ src/context/GlobalStateContext.jsx
@@ -35,7 +35,7 @@ const GlobalStateContext = ({ children }) => {
   const [cajaFilter, setCajaFilter] = useState(false)
   const [sueltoFilter, setSueltoFilter] = useState(false)
 
-  const [formEnvioPage, setFormEnvioPage] = useState(2)
+  const [formEnvioPage, setFormEnvioPage] = useState(0)
   const [direccionSelect, setDireccionSelect] = useState("default")
   const [direccionName, setDireccionName] = useState(null)
   const [direccionAutoSelect, setDireccionAutoSelect] = useState(false)
